4d1821a376801e5ff106396961d5ef00f4779d98
equal-l2 <eng.equall2@gmail.com>
2019-10-07 12:48:07 +0900

    Initial commit

---
+++ TestCases/実験08（2019.10.4）/CodeTest.c
@@ -0,0 +1,26 @@
+// コード生成テスト
+//	if, while, input, outputの文法が自分のと異なる場合は、
+//	自分の文法に合うように直して使うこと。
+//
+//	ラベルを置く位置、分岐命令の後ろのジャンプ先ラベル名が合っているか確認する
+
+int a;
+
+if (false) {
+   a=3;
+}
+
+if (true) {
+   a=1;
+} else {
+   a=2;
+}
+
+while (true) {
+   input a;
+   while (false) {
+      output a;
+   }
+   a=4;
+}
+
